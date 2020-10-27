8ea0a409c7da536b1ff99ee4d2028dd0637d76d2
Thiago Galeno <thiagogaleno1301@hotmail.com>
2020-10-27 15:19:56 -0300

    seeders concluido

--- database/seeds/ClientsTableSeeder.php
+++ database/seeds/ClientsTableSeeder.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Adress;
 use App\Client;
+use App\Phone;
+use App\User;
+use Faker\Factory as Faker;
 
 class ClientsTableSeeder extends Seeder
 {
@@ -12,6 +16,35 @@ class ClientsTableSeeder extends Seeder
      */
     public function run()
     {
-        Client::create([]);
+        $faker = Faker::create();
+        $users = User::all();
+        $i = 0;
+        while ($i < 2) {
+            foreach ($users as $user) {
+                $adress = Adress::create([
+                    'state' => $faker->stateAbbr,
+                    'city' => $faker->city,
+                    'street' => $faker->address,
+                    'zip_code' => strval($faker->numberBetween($min = 1000000, $max = 9999999)),
+                    'number' => strval($faker->randomDigit)
+                ]);
+                $phone = Phone::create([
+                    'mobile' => strval($faker->numberBetween($min = 990000000, $max = 999999999)),
+                    'landline' => strval($faker->numberBetween($min = 30000000, $max = 39999999)),
+                ]);
+
+                Client::create(
+                    [
+                        'name' => $faker->name,
+                        'birth_date' => strval($faker->date($format = 'Y-m-d', $max = 'now')),
+                        'gender' => 'male',
+                        'adress_id' => $adress->id,
+                        'phone_id' => $phone->id,
+                        'salesman_id' => $user->id
+                    ]
+                );
+            }
+            $i++;
+        }
     }
 }
